32ba9ef659049c8d7023a9e1fc470841c0ffcfc1
Miguel Medrano <>
2021-09-21 14:47:29 -0400

    Commit 1 not complete code

---
+++ Week_4_ApplicationCode/src/main/java/com/mycompany/week_4_applicationcode/NewClass.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.week_4_applicationcode;
+
+
+public class NewClass {
+    
+
+    int age = 10; 
+    
+    public static void main(String [] args) {
+        Helper nc1 = new Helper();
+        
+        NewClass ncl = new NewClass();
+        System.out.println("The age is " + ncl.cube(4));
+    }
+}
+
